17e9029e47d848082eba054c758e224bad8c6779
Tom <58078313+reed-tom@users.noreply.github.com>
2021-09-24 09:51:40 -0400

    updated all code indentation tabs with spaces to reduce any futher merge conflicts between forked projects

--- src/layerInfo/MapComponent.jsx
+++ src/layerInfo/MapComponent.jsx
@@ -18,66 +18,66 @@ const scaleLineControl = new ScaleLine();
 
 // STYLES
 const styles = {
-	poly: new Style({
-		stroke: new Stroke({
-			width: 4,
-			color: [255, 0, 0, 0.8],
-		}),
-	}),
+  poly: new Style({
+    stroke: new Stroke({
+      width: 4,
+      color: [255, 0, 0, 0.8],
+    }),
+  }),
 };
 
 class MapComponent extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {};
-	}
+    this.state = {};
+  }
 
-	componentDidMount() {
-		// TWEEK TO CENTER MAP DEPENDING ON MODE
-		const centerCoords = [-8875141.45, 5543492.45];
+  componentDidMount() {
+    // TWEEK TO CENTER MAP DEPENDING ON MODE
+    const centerCoords = [-8875141.45, 5543492.45];
 
-		var map = new Map({
-			controls: defaultControls().extend([scaleLineControl, new FullScreen()]),
-			layers: [
-				new TileLayer({
-					source: new OSM(),
-				}),
-			],
-			target: "map",
-			view: new View({
-				center: centerCoords,
-				zoom: 8,
-			}),
-		});
+    var map = new Map({
+      controls: defaultControls().extend([scaleLineControl, new FullScreen()]),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: "map",
+      view: new View({
+        center: centerCoords,
+        zoom: 8,
+      }),
+    });
 
-		window.map = map;
+    window.map = map;
 
-		// CREATE EXTENT
-		//var extent = [minlon, minlat, maxlon, maxlat];
-		// var extent = [-8969156.9680, 5382677.0565, -8765834.4727, 5702183.8348];
-		// var polygon = PolygonFromExtent(extent);
-		// var bufferPolygon = BufferExtent(extent, 5000);
-		var polygon = PolygonFromExtent(this.props.extent);
-		var bufferPolygon = BufferExtent(this.props.extent, 5000);
-		var feature = new Feature(polygon);
-		var extentLayer = new VectorLayer({
-			style: styles.poly,
-			source: new VectorSource({
-				features: [feature],
-			}),
-		});
-		map.getView().fit(bufferPolygon, map.getSize());
-		map.addLayer(extentLayer);
-	}
+    // CREATE EXTENT
+    //var extent = [minlon, minlat, maxlon, maxlat];
+    // var extent = [-8969156.9680, 5382677.0565, -8765834.4727, 5702183.8348];
+    // var polygon = PolygonFromExtent(extent);
+    // var bufferPolygon = BufferExtent(extent, 5000);
+    var polygon = PolygonFromExtent(this.props.extent);
+    var bufferPolygon = BufferExtent(this.props.extent, 5000);
+    var feature = new Feature(polygon);
+    var extentLayer = new VectorLayer({
+      style: styles.poly,
+      source: new VectorSource({
+        features: [feature],
+      }),
+    });
+    map.getView().fit(bufferPolygon, map.getSize());
+    map.addLayer(extentLayer);
+  }
 
-	render() {
-		return (
-			<div>
-				<div id="map"></div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div id="map"></div>
+      </div>
+    );
+  }
 }
 
 export default MapComponent;
--- src/sidebar/components/toc/toc-folder-view/TOCFolderView.jsx
+++ src/sidebar/components/toc/toc-folder-view/TOCFolderView.jsx
@@ -7,40 +7,40 @@ import GroupItem from "./GroupItem.jsx";
 import * as helpers from "../../../../helpers/helpers";
 
 class TOCFolderView extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		return nextProps.visible || this.props.visible;
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.visible || this.props.visible;
+  }
 
-	render() {
-		return (
-			<div className={this.props.visible ? "" : "sc-hidden"} id="sc-toc-simcoe-folder-view-container-main">
-				<div className="toc-group-list">
-					{this.props.layerGroups.map((group) => (
-						<GroupItem
-							key={this.props.id + "-group-" + helpers.getHash(group.value)}
-							id={this.props.id + "-group-" + helpers.getHash(group.value)}
-							group={group}
-							searchText={this.props.searchText}
-							sortAlpha={this.props.sortAlpha}
-							panelOpen={group.panelOpen}
-							saveLayerOptions={this.props.saveLayerOptions[group.value]}
-							onLayerChange={this.props.onLayerChange}
-							onLegendToggle={this.props.onLegendToggle}
-							onLayerVisibilityGroup={this.props.onLayerVisibilityGroup}
-							onGroupFolderToggle={this.props.onGroupFolderToggle}
-							onSliderChange={this.props.onSliderChange}
-							tocVisible={this.props.visible}
-							onLayerOptionsClick={this.props.onLayerOptionsClick}
-						/>
-					))}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className={this.props.visible ? "" : "sc-hidden"} id="sc-toc-simcoe-folder-view-container-main">
+        <div className="toc-group-list">
+          {this.props.layerGroups.map((group) => (
+            <GroupItem
+              key={this.props.id + "-group-" + helpers.getHash(group.value)}
+              id={this.props.id + "-group-" + helpers.getHash(group.value)}
+              group={group}
+              searchText={this.props.searchText}
+              sortAlpha={this.props.sortAlpha}
+              panelOpen={group.panelOpen}
+              saveLayerOptions={this.props.saveLayerOptions[group.value]}
+              onLayerChange={this.props.onLayerChange}
+              onLegendToggle={this.props.onLegendToggle}
+              onLayerVisibilityGroup={this.props.onLayerVisibilityGroup}
+              onGroupFolderToggle={this.props.onGroupFolderToggle}
+              onSliderChange={this.props.onSliderChange}
+              tocVisible={this.props.visible}
+              onLayerOptionsClick={this.props.onLayerOptionsClick}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 export default TOCFolderView;
